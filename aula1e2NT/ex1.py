9fb7659a9de155563917dbde1dfc53083d69dbef
Leo Ribeiro <111469440+Leoribeiro61@users.noreply.github.com>
2024-04-05 20:46:51 -0300

    Add files via upload

--- aula1e2NT/ex1.py
+++ aula1e2NT/ex1.py
@@ -1,1 +1,15 @@
+def operacoes_com_numeros():
+    a = int(input("Insira o primeiro número inteiro: "))
+    b = int(input("Insira o segundo número inteiro: "))
 
+    soma = a + b
+    produto = a * b
+    diferenca = abs(a - b)
+    divisao = a / b if b != 0 else "Divisão por zero não é permitida"
+
+    print("Soma:", soma)
+    print("Produto:", produto)
+    print("Diferença:", diferenca)
+    print("Divisão:", divisao)
+
+operacoes_com_numeros()
