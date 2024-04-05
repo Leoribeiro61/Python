fdcfef7b6d8424093b1180adefe2ddb8ce1671aa
Leo Ribeiro <111469440+Leoribeiro61@users.noreply.github.com>
2024-04-05 20:48:40 -0300

    Delete aula1e2NT directory

--- aula1e2NT/ex14.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Digite um número inteiro não negativo: "))
-resultado = 1
-
-for count in range(1, numero + 1):
-    resultado *= count
-
-print(f"O fatorial de {numero} é {resultado}")
