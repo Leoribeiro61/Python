8fc942865ab454cc702c2cc6d5d91fa33f75608a
Leo Ribeiro <111469440+Leoribeiro61@users.noreply.github.com>
2024-05-24 21:46:59 -0300

    Add files via upload

---
+++ pygame/ship.py
@@ -0,0 +1,25 @@
+import pygame
+class Ship:
+
+    def __init__(self, screen):
+
+        self.screen = screen
+        self.image = pygame.image.load('images/nave-espacial.png')
+        self.image_size = (70,70)
+        self.image = pygame.transform.scale(self.image, self.image_size)
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.moving_rigth = False
+        self.moving_left = False
+
+        def update(self):
+            if self.moving_rigth and self.rect.rigth < self.screen_rect.rigth:
+                self.rect.centerx +=1
+            if self.moving_left and self.rect.left>0:
+                self.rect.centerx -=1
+                
+        def blitme(self):
+
+            self.screen.blit(self.image, self.rect)
